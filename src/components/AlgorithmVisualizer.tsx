8d23957e04ab106ca421875810f20bebd177c3ce
Adishtienmetz <adisht135@gmail.com>
2024-06-17 06:51:34 +0300

    removed unused import

--- src/components/AlgorithmVisualizer.tsx
+++ src/components/AlgorithmVisualizer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef} from 'react';
 import ArrayVisualizer from './ArrayVisualizer';
 import { handleBubbleSort, handleMergeSort, handleQuickSort } from '../handlers/sortingHandlers';
 import AlgorithmCode from './AlgorithmCode';
